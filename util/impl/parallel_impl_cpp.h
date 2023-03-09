d55fc71b65a046773e5f36ef97c63fe7cd3f52ff
Xayah-Hina <ycxiang14sep@gmail.com>
2023-03-09 19:36:13 +0800

    udpate parallel

--- util/impl/parallel_impl_cpp.h
+++ util/impl/parallel_impl_cpp.h
@@ -18,18 +18,59 @@ void parallelFor(IndexType begin_index, IndexType end_index, const Function &fun
 }
 
 template<typename IndexType, typename Function>
-void parallelRangeFor(IndexType begin_index, IndexType end_index, const Function &function, ExecutionPolicy policy) {}
+void parallelRangeFor(IndexType begin_index, IndexType end_index, const Function &function, ExecutionPolicy policy)
+{
+	if (begin_index > end_index)
+		return;
+
+	function(begin_index, end_index);
+}
 
 template<typename IndexType, typename Function>
-void parallelFor(IndexType begin_indexX, IndexType end_indexX, IndexType begin_indexY, IndexType end_indexY, const Function &function, ExecutionPolicy policy) {}
+void parallelFor(IndexType begin_indexX, IndexType end_indexX, IndexType begin_indexY, IndexType end_indexY, const Function &function, ExecutionPolicy policy)
+{
+	parallelFor(begin_indexY, end_indexY,
+				[&](IndexType j)
+				{
+					for (IndexType i = begin_indexX; i < end_indexX; ++i)
+						function(i, j);
+				},
+				policy);
+}
 
 template<typename IndexType, typename Function>
-void parallelRangeFor(IndexType begin_indexX, IndexType end_indexX, IndexType begin_indexY, IndexType end_indexY, const Function &function, ExecutionPolicy policy) {}
+void parallelRangeFor(IndexType begin_indexX, IndexType end_indexX, IndexType begin_indexY, IndexType end_indexY, const Function &function, ExecutionPolicy policy)
+{
+	parallelRangeFor(begin_indexY, end_indexY,
+					 [&](IndexType jBegin, IndexType jEnd)
+					 {
+						 function(begin_indexX, end_indexX, jBegin, jEnd);
+					 },
+					 policy);
+}
 
 template<typename IndexType, typename Function>
-void parallelFor(IndexType begin_indexX, IndexType end_indexX, IndexType begin_indexY, IndexType end_indexY, IndexType begin_indexZ, IndexType end_indexZ, const Function &function, ExecutionPolicy policy) {}
+void parallelFor(IndexType begin_indexX, IndexType end_indexX, IndexType begin_indexY, IndexType end_indexY, IndexType begin_indexZ, IndexType end_indexZ, const Function &function, ExecutionPolicy policy)
+{
+	parallelFor(begin_indexZ, end_indexZ,
+				[&](IndexType k)
+				{
+					for (IndexType j = begin_indexY; j < end_indexY; ++j)
+						for (IndexType i = begin_indexX; i < end_indexX; ++i)
+							function(i, j, k);
+				},
+				policy);
+}
 
 template<typename IndexType, typename Function>
-void parallelRangeFor(IndexType begin_indexX, IndexType end_indexX, IndexType begin_indexY, IndexType end_indexY, IndexType begin_indexZ, IndexType end_indexZ, const Function &function, ExecutionPolicy policy) {}
+void parallelRangeFor(IndexType begin_indexX, IndexType end_indexX, IndexType begin_indexY, IndexType end_indexY, IndexType begin_indexZ, IndexType end_indexZ, const Function &function, ExecutionPolicy policy)
+{
+	parallelRangeFor(begin_indexZ, end_indexZ,
+					 [&](IndexType kBegin, IndexType kEnd)
+					 {
+						 function(begin_indexX, end_indexX, begin_indexY, end_indexY, kBegin, kEnd);
+					 },
+					 policy);
+}
 }
 #endif //HINAPE_PARALLEL_IMPL_CPP_H
