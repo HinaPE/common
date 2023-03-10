d7e58a98d9662833d52ee257caf915a82882dbd8
Xayah-Hina <ycxiang14sep@gmail.com>
2023-03-10 15:50:41 +0800

    add igl::marching cubes

---
+++ util/marching_cubes.cpp
@@ -0,0 +1,1 @@
+#include "marching_cubes.h"
---
+++ util/marching_cubes.h
@@ -0,0 +1,14 @@
+#ifndef HINAPE_MARCHING_CUBES_H
+#define HINAPE_MARCHING_CUBES_H
+
+// Copyright (c) 2023 Xayah Hina
+// MPL-2.0 license
+
+namespace HinaPE::Util
+{
+class MarchingCubes
+{
+};
+} // namespace HinaPE::Util
+
+#endif //HINAPE_MARCHING_CUBES_H
