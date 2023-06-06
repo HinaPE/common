e0c7f968db70070aff09cb8a0e1cfcb7fc5113ca
Xayah-Hina <ycxiang14sep@gmail.com>
2023-06-06 15:17:17 +0800

    compatible to cpp-17

--- common.h
+++ common.h
@@ -33,9 +33,5 @@ public:
 	auto operator=(CopyDisable &&) -> CopyDisable & = default;
 };
 }  // namespace HinaPE
-// @formatter:off
-template<typename T> auto is(const auto *src) -> bool { return dynamic_cast<const T *>(src) != nullptr; }
-template<typename T> auto as(auto *src) -> T * { return dynamic_cast<T *>(src); }
-// @formatter:on
 
 #endif //HINAPE_COMMON_H
